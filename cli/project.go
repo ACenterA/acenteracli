95ec99bb1135404f860d234eb7fa62a9faf4318e
Francis Lavalliere <Francis.Lavalliere@acentera.com>
2019-09-03 16:40:12 -0400

    added projects

---
+++ cli/project.go
@@ -0,0 +1,79 @@
+package cli
+
+import (
+	"encoding/json"
+)
+
+/*	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"gopkg.in/yaml.v2"
+        errors "github.com/pkg/errors"
+
+	"github.com/alecthomas/chroma/quick"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/h2non/gentleman.v2"
+	"gopkg.in/h2non/gentleman.v2/plugins/auth"
+	config "github.com/wallix/awless/config"
+	global "github.com/wallix/awless/global"
+	logger "github.com/wallix/awless/logger"
+	"gopkg.in/h2non/gentleman.v2/context"
+*/
+
+type ProjectApi struct {
+	Id   string `json:"projectId"`
+	Name string `json:"name"`
+}
+
+func (api *ProjectApi) GetProjects() (map[string]ProjectApi, error) {
+	var resp map[string]ProjectApi
+	itm, err := API().GetByKey("/customer/v1/websites/me", "projects")
+	if err != nil {
+		return nil, err
+	}
+	jsonData, err := json.Marshal(itm)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(jsonData, &resp); err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+/*
+func (api *APIInternal) Project() (map[string]interface{}, err) {
+	url := "/customer/v1/websites/me"
+	req := cli.API().Path(url).Get()
+	resp, err := req.Do()
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "Request failed"))
+	}
+	var decoded map[string]interface{}
+	if resp.StatusCode < 400 {
+		if err := cli.UnmarshalResponse(resp, &decoded); err != nil {
+			fmt.Println(errors.Wrap(err, "Unmarshalling response failed"))
+		}
+	} else {
+		fmt.Println(errors.Errorf("HTTP %d: %s", resp.StatusCode, resp.String()))
+	}
+	if resp.StatusCode <= 208 && resp.StatusCode >= 200 {
+		logger.Verbosef("Got decoded of %s", decoded)
+		if _, ok := decoded["project"]; ok {
+			fmt.Printf("Username: %s, Id: %s\n", decoded["contactEmail"], decoded["accountId"])
+		}
+		return decoded
+	} else {
+		logger.Error("Invalid access token.")
+	}
+}
+*/
